a58ad95bed3c158bf05c2243b2b7b33df7f9292d
yzhonda <277689182+yzhonda@users.noreply.github.com>
2026-05-17 20:46:19 +0900

    ccsl 1.0.12

--- Formula/ccsl.rb
+++ Formula/ccsl.rb
@@ -3,8 +3,8 @@ class Ccsl < Formula
 
   desc "Enhanced status line for Claude Code"
   homepage "https://github.com/yzhonda/statusline"
-  url "https://files.pythonhosted.org/packages/3c/a3/52a0dbf1dd780a94c4a46fa39050b51633525e47961a1e6545caf96ab0e0/ccsl-1.0.11.tar.gz"
-  sha256 "e7924516912bfdd42a414dc8f09e2d83c2e560f4b838fb9d886194e5bd25f9e4"
+  url "https://files.pythonhosted.org/packages/1d/6e/4d84b4e8e291d8670be4f5ab9d574ac91beb105ba2a7780b0e55db481aed/ccsl-1.0.12.tar.gz"
+  sha256 "705ba2ab1b064935c20ab2ae4810dd9119c88340e77cc78386a429f40bac6abf"
   license "MIT"
 
   depends_on "python@3.12"
