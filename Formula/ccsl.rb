801a5474253ce241ba955119747172cdac65c637
yzhonda <277689182+yzhonda@users.noreply.github.com>
2026-05-17 20:37:30 +0900

    chore: update homepage to yzhonda/statusline

--- Formula/ccsl.rb
+++ Formula/ccsl.rb
@@ -2,7 +2,7 @@ class Ccsl < Formula
   include Language::Python::Virtualenv
 
   desc "Enhanced status line for Claude Code"
-  homepage "https://github.com/usedhonda/statusline"
+  homepage "https://github.com/yzhonda/statusline"
   url "https://files.pythonhosted.org/packages/3c/a3/52a0dbf1dd780a94c4a46fa39050b51633525e47961a1e6545caf96ab0e0/ccsl-1.0.11.tar.gz"
   sha256 "e7924516912bfdd42a414dc8f09e2d83c2e560f4b838fb9d886194e5bd25f9e4"
   license "MIT"
